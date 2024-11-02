3c48483e0cef6f60b854056cbd4a0f995ea241eb
BeverllyHanif <145525619+Beverlly1728@users.noreply.github.com>
2024-11-02 19:36:15 +0700

    Add files via upload

---
+++ Football24/app/src/main/java/id/example/shashank/football24/MainActivity.java
@@ -0,0 +1,14 @@
+package id.example.shashank.football24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
